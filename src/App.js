c24dba2e694ce1f543ff129632cea847bcc085d1
Justin Case <justin.case1061@gmail.com>
2024-11-18 14:59:10 -0600

    properly displaying map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import ZipInputBar from './components/ZipInputBar/ZipInputBar.jsx'
 import MapContainer from './components/MapContainer/MapContainer.jsx'
+import { APIProvider } from '@vis.gl/react-google-maps'
+
 
 const App = () => {
+
+
   return (
-    <div>
-      <ZipInputBar />
-      <MapContainer />
+    <div id='app-container'>
+      <APIProvider apiKey={process.env.REACT_APP_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+        <ZipInputBar />
+        <MapContainer />
+      </APIProvider>
     </div>
   );
 }
--- src/components/MapContainer/MapContainer.jsx
+++ src/components/MapContainer/MapContainer.jsx
@@ -1,21 +1,39 @@
-import { Loader } from "@googlemaps/js-api-loader"
-import {APIProvider} from '@vis.gl/react-google-maps'
-import {useEffect} from 'react'
+import { Loader } from "@googlemaps/js-api-loader";
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+} from "@vis.gl/react-google-maps";
 
 const MapContainer = () => {
 
-  useEffect(() => {
-    console.log(process.env.REACT_APP_API_KEY)
-  }, [])
-
+  const mapStyle = {
+    display: 'block',
+    height: '80vh',
+    width: '80vw',
+    margin: '5vw 10vw 0 10vw'
+  }
 
   return (
-    <APIProvider apiKey={process.env.REACT_APP_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
-      <div>
-        <div>this is the map container</div>
-      </div>
+    <APIProvider
+      apiKey={process.env.REACT_APP_API_KEY}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+        <Map
+          style={mapStyle}
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom
+            )
+          }
+        ></Map>
     </APIProvider>
-  )
-}
+  );
+};
 
-export default MapContainer
+export default MapContainer;
