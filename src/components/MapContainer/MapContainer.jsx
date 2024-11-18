0b10af54e01884568c121898a3db657ff6edd057
Justin Case <justin.case1061@gmail.com>
2024-11-18 14:15:01 -0600

    importing google maps api

--- src/components/MapContainer/MapContainer.jsx
+++ src/components/MapContainer/MapContainer.jsx
@@ -1,12 +1,20 @@
-
+import { Loader } from "@googlemaps/js-api-loader"
+import {APIProvider} from '@vis.gl/react-google-maps'
+import {useEffect} from 'react'
 
 const MapContainer = () => {
 
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_KEY)
+  }, [])
+
 
   return (
-    <div>
-      <div>this is the map container</div>
-    </div>
+    <APIProvider apiKey={process.env.REACT_APP_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+      <div>
+        <div>this is the map container</div>
+      </div>
+    </APIProvider>
   )
 }
 
